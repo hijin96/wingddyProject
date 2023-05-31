f2a48927969c0a2f79164949c928f9be8910f6b3
Sunj33 <134987188+Sunj33@users.noreply.github.com>
2023-05-31 15:48:32 +0900

    멤버컨트롤러 원상복구 - 선재-

--- finalProject/src/main/java/com/kh/wingddy/member/controller/MemberController.java
+++ finalProject/src/main/java/com/kh/wingddy/member/controller/MemberController.java
@@ -1,12 +1,5 @@
 package com.kh.wingddy.member.controller;
 
 public class MemberController {
-
-	public static void main(String[] args) {
-		
-		System.out.println("22");
-	
-		System.out.println("oo");
-	}
 	
 }
