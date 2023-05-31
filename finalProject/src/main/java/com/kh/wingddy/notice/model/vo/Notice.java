0542a1baecc9a67358c2221a8544b83c45d93589
MarchTrack <134987182+MarchTrack@users.noreply.github.com>
2023-05-31 16:04:17 +0900

    notice VO -연대-

--- finalProject/src/main/java/com/kh/wingddy/notice/model/vo/Notice.java
+++ finalProject/src/main/java/com/kh/wingddy/notice/model/vo/Notice.java
@@ -1,5 +1,16 @@
 package com.kh.wingddy.notice.model.vo;
 
+import lombok.Data;
+
+@Data
 public class Notice {
+	
+	private int noticeNo; //NOTICE_NO	NUMBER
+	private int noticeWriter; //NOTICE_WRITER	NUMBER
+	private String noticeTitle; //NOTICE_TITLE	VARCHAR2(300 BYTE)
+	private String noticeContent; //NOTICE_CONTENT	VARCHAR2(1000 BYTE)
+	private String createDate; //CREATE_DATE	DATE
+	private int noticeCount; //NOTICE_COUNT	NUMBER
+	private String noticeStatus; //NOTICE_STATUS	CHAR(1 BYTE)
 
 }
