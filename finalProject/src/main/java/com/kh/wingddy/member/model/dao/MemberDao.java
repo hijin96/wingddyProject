bb6e170e04d07b76a2f3491bf6211422e5fd9f36
MarchTrack <134987182+MarchTrack@users.noreply.github.com>
2023-06-01 09:25:07 +0900

    member -연대-

--- finalProject/src/main/java/com/kh/wingddy/member/model/dao/MemberDao.java
+++ finalProject/src/main/java/com/kh/wingddy/member/model/dao/MemberDao.java
@@ -1,5 +1,15 @@
 package com.kh.wingddy.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.wingddy.member.model.vo.Member;
+
+@Repository
 public class MemberDao {
 
+	public Member loginMember(SqlSessionTemplate sqlSession, Member m) {
+		
+		return sqlSession.selectOne("memberMapper.loginMember", m);
+	}
 }
