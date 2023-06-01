0786c797c021fc1ec3860428b7bb433c711a16a3
MarchTrack <134987182+MarchTrack@users.noreply.github.com>
2023-06-01 09:57:23 +0900

    로그인 -연대-

--- finalProject/src/main/java/com/kh/wingddy/member/model/service/MemberServiceImpl.java
+++ finalProject/src/main/java/com/kh/wingddy/member/model/service/MemberServiceImpl.java
@@ -2,10 +2,12 @@ package com.kh.wingddy.member.model.service;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kh.wingddy.member.model.dao.MemberDao;
 import com.kh.wingddy.member.model.vo.Member;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
 	@Autowired
