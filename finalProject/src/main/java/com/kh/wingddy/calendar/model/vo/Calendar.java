df5d8dc697256d4fc71b6c88c7d1013980882525
sasagwa <nanava0317@gmail.com>
2023-06-01 15:34:05 +0900

    캘린더 파일 기본 세팅 -채은-
    Class, mapper, mybatis-config 설정 등

--- finalProject/src/main/java/com/kh/wingddy/calendar/model/vo/Calendar.java
+++ finalProject/src/main/java/com/kh/wingddy/calendar/model/vo/Calendar.java
@@ -1,8 +1,15 @@
 package com.kh.wingddy.calendar.model.vo;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class Calendar {
-	
+	private int scheduleNo;
+	private int memberNo;
+	private String schedule;
+	private Date startDate;
+	private Date endDate;
+	private String color;
 }
