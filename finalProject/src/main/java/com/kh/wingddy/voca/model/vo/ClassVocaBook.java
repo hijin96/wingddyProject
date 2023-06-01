ca1e0225cdff743d7c780ea9c30388a92f861f68
Sunj33 <134987188+Sunj33@users.noreply.github.com>
2023-06-01 12:20:03 +0900

    voca추가 -선재-

---
+++ finalProject/src/main/java/com/kh/wingddy/voca/model/vo/ClassVocaBook.java
@@ -0,0 +1,12 @@
+package com.kh.wingddy.voca.model.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor 
+public class ClassVocaBook extends VocaBook{
+
+	private int classNo;
+	private String className;
+}
--- finalProject/src/main/java/com/kh/wingddy/voca/model/vo/Voca.java
+++ finalProject/src/main/java/com/kh/wingddy/voca/model/vo/Voca.java
@@ -1,19 +1,11 @@
 package com.kh.wingddy.voca.model.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor @Builder
+@Data
 public class Voca {
 
 	private int bookNo;
-	private int memberNo;
-	private int classNo;
-	private String bookName; 
-	private String memberName;
-	private String className;
 	private String vocaEnglish;
 	private String vocaKorean;
 }
