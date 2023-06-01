5e2546679cd0e0738a4f152a7e0f0d4f1b3d348a
Sunj33 <134987188+Sunj33@users.noreply.github.com>
2023-06-01 10:30:46 +0900

    sideBar및 voca페이지 수정 -선재-

--- finalProject/src/main/java/com/kh/wingddy/voca/controller/VocaController.java
+++ finalProject/src/main/java/com/kh/wingddy/voca/controller/VocaController.java
@@ -1,5 +1,14 @@
 package com.kh.wingddy.voca.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class VocaController {
 
+	@RequestMapping("main.vc")
+	public String vocamainView() {
+		return "voca/vocaMain";
+	}
+	
 }
