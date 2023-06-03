d434dd374e097bdde51d8f49a6fa771e7bf0f60f
hijin96 <yunkmink@gmail.com>
2023-06-03 14:17:20 +0900

    commit- 희진
    commit- 희진 page

---
+++ finalProject/src/main/java/com/kh/wingddy/common/model/vo/PageInfo.java
@@ -0,0 +1,28 @@
+package com.kh.wingddy.common.model.vo;
+
+import lombok.Data;
+
+@Data 
+public class PageInfo {
+	private int listCount;
+	private int currentPage;
+	private int boardLimit;
+	private int pageLimit;
+	
+	private int maxPage;
+	private int startPage;
+	private int endPage;
+	
+	public PageInfo(int listCount, int currentPage, int boardLimit, int pageLimit, int maxPage, int startPage,
+			int endPage) {
+		super();
+		this.listCount = listCount;
+		this.currentPage = currentPage;
+		this.boardLimit = boardLimit;
+		this.pageLimit = pageLimit;
+		this.maxPage = maxPage;
+		this.startPage = startPage;
+		this.endPage = endPage;
+	}
+	
+}
