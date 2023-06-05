e1a0fe57717483f855e3cdf27451ba32210a2eb3
seheeShin00 <tpgml3140@gmail.com>
2023-06-05 18:26:28 +0900

    alphabet - sehee

--- finalProject/src/main/java/com/kh/wingddy/alphabetMarket/model/dao/AlphabetDao.java
+++ finalProject/src/main/java/com/kh/wingddy/alphabetMarket/model/dao/AlphabetDao.java
@@ -2,6 +2,7 @@ package com.kh.wingddy.alphabetMarket.model.dao;
 
 import java.util.ArrayList;
 
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +11,8 @@ import com.kh.wingddy.alphabetMarket.model.vo.AlphabetMarket;
 @Repository
 public class AlphabetDao {
 	
-	public void ajaxSelectMarketList(SqlSessionTemplate sqlSession, AlphabetMarket am){
-		//return sqlSession.selectList(am);
+	public ArrayList<alphabetMarket> ajaxSelectMarketList(SqlSessionTemplate sqlSession, AlphabetMarket am){
+		return sqlSession.selectlist("alphabetMapper.selectList", )
 	}
 
 }
